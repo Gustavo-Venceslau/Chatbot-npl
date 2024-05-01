3f05396f8e2f2ed0bc17e264090d8a32cd53b538
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2024-04-30 21:43:38 -0300

    training model created and saved

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,6 +1,6 @@
 import nltk
-nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+import numpy as np
 
 stemmer = PorterStemmer()
 
@@ -11,4 +11,11 @@ def stem(word):
 	return stemmer.stem(word.lower())
 
 def bag_of_words(tokenized_setence, all_words):
-	pass
+	tokenized_setence = [stem(w) for w in tokenized_setence]
+
+	bag = np.zeros(len(all_words), dtype=np.float32)
+	for idx, w in enumerate(all_words):
+		if w in tokenized_setence:
+			bag[idx] = 1.0
+
+	return bag
